703b71a485d0c13f64c37a6a26cec7e1a7eaeb4d
lap14tclc2 <honaz140@gmail.com>
2019-01-08 17:04:01 +0700

    UP

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #redirect to new page
+      log_in user
+      redirect_to user_url(user)
+    else
+      # display in rendered page and it will disappered in next request
+      flash.now[:danger] = "Invalid information!!!"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
